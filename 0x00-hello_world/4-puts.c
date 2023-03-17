b458a0e7fa26e2df9af580e589f9e0a42dbc065f
T-Lot-Lego <tlotlego.lefoka@gmail.com>
2023-03-16 20:45:43 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Hell puts program to print a line
+ * return 0 (End program)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
