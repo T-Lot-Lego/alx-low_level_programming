b89dc6cb999d0dad03c735d020850065e76387d9
T-Lot-Lego <tlotlego.lefoka@gmail.com>
2023-03-20 15:12:03 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lu;
+
+	for (lu = 'a'; lu <= 'z'; lu++)
+	{
+		putchar(lu);
+	}
+	for (lu = 'A'; lu <= 'Z'; lu++)
+	{
+		putchar(lu);
+	}
+	putchar('\n');
+	return (0);
+}
