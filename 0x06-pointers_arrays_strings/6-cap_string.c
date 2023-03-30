185a05a0049ccf485334ce2aaca59e1804522506
T-Lot-Lego <tlotlego.lefoka@gmail.com>
2023-03-29 19:07:47 -0700

    6-cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,30 +7,30 @@
  */
 char *cap_string(char *str)
 {
-	int cap = 0;
+	int index = 0;
 
-	while (str[cap])
+	while (str[index])
 	{
-		while (!(str[cap] >= 'a' && str[cap] <= 'z'))
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 
-		if (str[cap - 1] == ' ' ||
-		    str[cap - 1] == '\t' ||
-		    str[cap - 1] == '\n' ||
-		    str[cap - 1] == ',' ||
-		    str[cap - 1] == ';' ||
-		    str[cap - 1] == '.' ||
-		    str[cap - 1] == '!' ||
-		    str[cap - 1] == '?' ||
-		    str[cap - 1] == '"' ||
-		    str[cap - 1] == '(' ||
-		    str[cap - 1] == ')' ||
-		    str[cap - 1] == '{' ||
-		    str[cap - 1] == '}' ||
-		    cap == 0)
-			str[cap] -= 32;
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
 
-		cap++;
+		index++;
 	}
 
 	return (str);
